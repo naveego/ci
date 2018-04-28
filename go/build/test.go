45637d21cd5357c1fc25347778695d1064d3f157
Derek Smith <derek.smith@naveego.com>
2018-04-28 14:02:50 -0400

    Updated Ginkgo test

--- go/build/test.go
+++ go/build/test.go
@@ -1,12 +1,25 @@
 package build
 
-import "github.com/magefile/mage/sh"
+import (
+	"fmt"
+	"strings"
 
-func GinkgoIntegration() error {
-	return sh.Run(
-		"ginkgo",
-		"--tags",
-		"integration",
+	"github.com/magefile/mage/sh"
+)
+
+// Ginkgo runs tests recursively
+func Ginkgo(tags []string) error {
+	// make sure we have ginkgo
+	err := sh.Run("go", "get", "github.com/onsi/ginkgo/ginkgo")
+	if err != nil {
+		return fmt.Errorf("Could not install ginkgo, %v", err)
+	}
+	err := sh.Run("go", "get", "github.com/onsi/gomega/...")
+	if err != nil {
+		return fmt.Errorf("Could not install gomega, %v", err)
+	}
+
+	args := []string{
 		"-r",
 		"--progress",
 		"--randomizeAllSpecs",
@@ -14,5 +27,17 @@ func GinkgoIntegration() error {
 		"--cover",
 		"--trace",
 		"--race",
-	)
+	}
+
+	if len(tags) > 0 {
+		args = append(args, "--tags")
+		args = append(args, strings.Join(tags, " "))
+	}
+
+	return sh.Run("ginkgo", args)
+}
+
+// GinkgoIntegration runs test recusively, but uses integration tag
+func GinkgoIntegration() error {
+	return Ginkgo([]string{"integration"})
 }
